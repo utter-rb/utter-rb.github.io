7384510b1a1d08d021441c0b0cb015a7c57ceb56
zotherstupidguy <zotherstupidguy@gmail.com>
2016-10-14 18:46:04 +0200

    adds modal

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,4 @@
-
-$("#fade").modal({
-  fadeDuration: 1000,
-  fadeDelay: 0.50
+$(document).ready(function(){
+  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+  $('.modal-trigger').leanModal();
 });
